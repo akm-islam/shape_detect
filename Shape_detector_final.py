88da86c6f6ef47f27d341b4d1040d8727e78ddc7
AKM Islam <mamunjahir.com@gmail.com>
2020-07-26 07:46:21 -0400

    added x and y

--- Shape_detector_final.py
+++ Shape_detector_final.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
 import numpy as np
 import cv2
 def getWidth(cnt):
@@ -19,13 +17,14 @@ def getXY(cnt):
     a=[]
     b=[]
     for i in cnt:
-        a.append(i[0][1])
-        b.append(i[0][0])
+        a.append(i[0][0])
+        b.append(i[0][1])
     #print("min",min(a),min(b))
     return (min(a),min(b))
 def getShape(img_url):
     img = cv2.imread(img_url)
     print(img.shape)
+    print()
     #----------------
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret,thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
@@ -36,12 +35,12 @@ def getShape(img_url):
         approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
         if len(approx)==4:
             if getXY(cnt)[0]!=0:
-                dict_to_return["div"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"xy":getXY(cnt)}
+                dict_to_return["div"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"x":getXY(cnt)[0],"y":getXY(cnt)[1]}
         elif len(approx)==7:
-            dict_to_return["carrosel"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"xy":getXY(cnt)}
+            dict_to_return["div"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"x":getXY(cnt)[0],"y":getXY(cnt)[1]}
         elif len(approx)==6:
-            dict_to_return["Img"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"xy":getXY(cnt)}
+            dict_to_return["div"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"x":getXY(cnt)[0],"y":getXY(cnt)[1]}
         elif len(approx)==8:
-            dict_to_return["Nav"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"xy":getXY(cnt)}
+            dict_to_return["div"+str(np.random.randint(10))]={"width":getWidth(cnt),"Height":getHeight(cnt),"x":getXY(cnt)[0],"1":getXY(cnt)[1]}
     return dict_to_return
 print(getShape('01.png'))
